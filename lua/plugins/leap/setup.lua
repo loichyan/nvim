f2a4d23f6db412a6f9868bc4fe2f1d8cc1008023
Loi Chyan <loichyan@foxmail.com>
2024-05-10 11:13:26 +0800

    feat(leap): consistent cursor stop

--- lua/plugins/leap/setup.lua
+++ lua/plugins/leap/setup.lua
@@ -3,7 +3,7 @@ return function(_, opts)
   require("leap.user").set_repeat_keys("<enter>", "<backspace>")
 
   local map = vim.keymap.set
-  map({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
-  map({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+  map({ "n", "x", "o" }, "s", "<Plug>(leap-forward-to)")
+  map({ "n", "x", "o" }, "S", "<Plug>(leap-backward-to)")
   map({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
 end
