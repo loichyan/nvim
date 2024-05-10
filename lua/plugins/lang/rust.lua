7dcfcfda7c8d498b5ea486febe60eeb94753157e
Loi Chyan <loichyan@foxmail.com>
2024-05-10 16:15:40 +0800

    fix(rustaceanvim): prevent rust-analyzer setup conflicts

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -6,7 +6,35 @@ return {
     cond = not vim.g.vscode,
     event = "User AstroFile",
     ft = { "rust" },
-    init = function() vim.g.rustaceanvim = require("plugins.lsp.servers").rust_analyzer end,
+    config = function()
+      vim.g.rustaceanvim = {
+        settings = {
+          ["rust-analyzer"] = {
+            check = { command = "clippy" },
+            rustfmt = { overrideCommand = { "rustfmt-nightly" } },
+            procMacro = { enable = true, attributes = { enable = true } },
+            typing = { autoClosingAngleBrackets = { enable = true } },
+            imports = { granularity = { enforce = true } },
+            buildScripts = { rebuildOnSave = true },
+          },
+        },
+        on_attach = function(_, bufnr)
+          local map = vim.keymap.set
+          map(
+            "n",
+            "<Leader>le",
+            "<Cmd>RustLsp expandMacro<CR>",
+            { desc = "Expand macro", buffer = bufnr }
+          )
+          map(
+            "n",
+            "<Leader>lc",
+            "<Cmd>RustLsp openCargo<CR>",
+            { desc = "Open Cargo.toml", buffer = bufnr }
+          )
+        end,
+      }
+    end,
   },
 
   {
--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -33,33 +33,6 @@ return {
   },
   nil_ls = {},
   pyright = {},
-  rust_analyzer = {
-    settings = {
-      ["rust-analyzer"] = {
-        check = { command = "clippy" },
-        rustfmt = { overrideCommand = { "rustfmt-nightly" } },
-        procMacro = { enable = true, attributes = { enable = true } },
-        typing = { autoClosingAngleBrackets = { enable = true } },
-        imports = { granularity = { enforce = true } },
-        buildScripts = { rebuildOnSave = true },
-      },
-    },
-    on_attach = function(_, bufnr)
-      local map = vim.keymap.set
-      map(
-        "n",
-        "<Leader>le",
-        "<Cmd>RustLsp expandMacro<CR>",
-        { desc = "Expand macro", buffer = bufnr }
-      )
-      map(
-        "n",
-        "<Leader>lc",
-        "<Cmd>RustLsp openCargo<CR>",
-        { desc = "Open Cargo.toml", buffer = bufnr }
-      )
-    end,
-  },
   taplo = {},
   tailwindcss = {},
   texlab = {},
