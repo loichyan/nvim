4c061cd1ff8b8830d13d913fd085a0e8eb31950f
Loi Chyan <loichyan@foxmail.com>
2024-05-12 17:27:38 +0800

    fix(catppuccin): add missing integrations

--- lua/pde/plugins/catppuccin.lua
+++ lua/pde/plugins/catppuccin.lua
@@ -2,15 +2,18 @@
 return {
   "catppuccin",
   opts = function(_, opts)
-    opts.flavour = "mocha"
-    local prev_highlights = opts.custom_highlights
-    opts.custom_highlights = function(c)
-      return require("deltavim.utils").merge({
-        LeapMatch = { fg = c.lavender, bold = true },
-        LeapLabelPrimary = { fg = c.red, bold = true },
-        LeapLabelSecondary = { fg = c.peach, bold = true },
-        LeapBackdrop = { fg = c.overlay0 },
-      }, prev_highlights and prev_highlights(c) or {})
-    end
+    local utils, prev_highlights = require "deltavim.utils", opts.custom_highlights
+    utils.deep_merge(opts, {
+      flavour = "mocha",
+      integrations = { diffview = true },
+      custom_highlights = function(c)
+        return utils.merge({
+          LeapMatch = { fg = c.lavender, bold = true },
+          LeapLabelPrimary = { fg = c.red, bold = true },
+          LeapLabelSecondary = { fg = c.peach, bold = true },
+          LeapBackdrop = { fg = c.overlay0 },
+        }, prev_highlights and prev_highlights(c) or {})
+      end,
+    })
   end,
 }
