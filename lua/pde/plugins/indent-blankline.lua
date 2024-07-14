4c5deb591cb3317efa9e299e358e4eb12cb3b427
Loi Chyan <loichyan@foxmail.com>
2024-07-14 12:39:17 +0800

    fix(indent-blank): it does not break NeoVim 0.9 anymore

--- lua/pde/plugins/indent-blankline.lua
+++ lua/pde/plugins/indent-blankline.lua
@@ -1,6 +1,2 @@
 ---@type LazyPluginSpec
-return {
-  "indent-blankline.nvim",
-  -- TODO: remove this in Neovim 0.10
-  commit = "3d08501caef2329aba5121b753e903904088f7e6",
-}
+return { "indent-blankline.nvim" }
