ebb37062d0158d548ccd2bc4e86d423625a0557d
Loi Chyan <loichyan@foxmail.com>
2024-07-20 17:55:40 +0800

    fix(vim-matchup): hrsh7th/nvim-cmp#1940

--- lua/pde/plugins/vim-matchup.lua
+++ lua/pde/plugins/vim-matchup.lua
@@ -2,8 +2,15 @@
 return {
   "andymass/vim-matchup",
   event = "User AstroFile",
-  dependencies = {
+  specs = {
     { "nvim-treesitter", opts = { matchup = { enable = true } } },
   },
-  opts = function() vim.g.matchup_matchparen_pumvisible = 0 end,
+  config = function(_, opts)
+    local ok, cmp = pcall(require, "cmp")
+    if ok then
+      cmp.event:on("menu_opened", function() vim.b.matchup_matchparen_enabled = false end)
+      cmp.event:on("menu_closed", function() vim.b.matchup_matchparen_enabled = true end)
+    end
+    require("match-up").setup(opts)
+  end,
 }
