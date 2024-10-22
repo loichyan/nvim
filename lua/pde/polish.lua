61fef793f8415c7567346ca3d0acce1260372365
Loi Chyan <loichyan@foxmail.com>
2024-10-22 14:49:57 +0800

    fix(polish): remove auto short links

--- lua/pde/polish.lua
+++ lua/pde/polish.lua
@@ -12,20 +12,3 @@ vim.filetype.add {
     justfile = "just",
   },
 }
-
-local prev_paste = vim.paste
----@param lines string[]
----@diagnostic disable-next-line: duplicate-set-field
-vim.paste = function(lines, ...)
-  for i, line in ipairs(lines) do
-    local _, _, s1, s2, hash, s4 = line:find "(https?://.+/)(%a+)(/%x+)(/.+)"
-    if
-      s2 == "blob" -- GitHub/GitLab
-      or s2 == "commit" -- Gitea
-      or s2 == "tree" -- sourcehut
-    then
-      lines[i] = s1 .. s2 .. hash:sub(1, 13) .. s4
-    end
-  end
-  prev_paste(lines, ...)
-end
