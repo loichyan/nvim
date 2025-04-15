ac6a69bf6ecdf813fd348d6370a986bf0e466f42
Loi Chyan <loichyan@foxmail.com>
2025-04-15 21:39:11 +0800

    feat: automatically configure rulers

--- lua/meowim/config/autocommands.lua
+++ lua/meowim/config/autocommands.lua
@@ -1,20 +1,29 @@
-vim.api.nvim_create_autocmd("FileType", {
+local au = vim.api.nvim_create_autocmd
+local trivial_files = {
+    ["checkhealth"] = true,
+    ["fzf"] = true,
+    ["git"] = true,
+    ["help"] = true,
+    ["lspinfo"] = true,
+    ["man"] = true,
+    ["nofile"] = true,
+    ["notify"] = true,
+    ["qf"] = true,
+    ["query"] = true,
+    ["quickfix"] = true,
+    ["startuptime"] = true,
+    ["vim"] = true,
+}
+local rulers = {
+    ["*"] = 80,
+    ["git"] = 72,
+    ["gitcommit"] = 72,
+    ["markdown"] = 100,
+}
+
+au("FileType", {
     desc = "Tweak trivial files",
-    pattern = {
-        "checkhealth",
-        "fzf",
-        "git",
-        "help",
-        "lspinfo",
-        "man",
-        "nofile",
-        "notify",
-        "qf",
-        "query",
-        "quickfix",
-        "startuptime",
-        "vim",
-    },
+    pattern = vim.tbl_keys(trivial_files),
     callback = function(args)
         vim.b.miniindentscope_disable = true
         vim.bo.buflisted = false
@@ -24,5 +33,17 @@ vim.api.nvim_create_autocmd("FileType", {
             "<Cmd>close<CR>",
             { desc = "Close current window", buffer = args.buf }
         )
+    end,
+})
+au("FileType", {
+    desc = "Configure rulers",
+    callback = function()
+        local ft = vim.bo.filetype
+        if trivial_files[ft] then
+            return
+        end
+        local width = rulers[ft] or rulers["*"]
+        vim.opt_local.textwidth = width
+        vim.opt_local.colorcolumn = { width }
     end,
 })
--- lua/meowim/config/options.lua
+++ lua/meowim/config/options.lua
@@ -1,2 +1,4 @@
+vim.opt.expandtab = true
 vim.opt.relativenumber = true
+vim.opt.tabstop = 4
 vim.diagnostic.config({ virtual_text = true })
