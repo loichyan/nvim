b65d4851ca51c7f19f515c654a161171ef48772b
Loi Chyan <loichyan@foxmail.com>
2025-04-17 18:16:05 +0800

    feat: enable more LSPs

--- lua/meowim/plugins/nvim-lspconfig.lua
+++ lua/meowim/plugins/nvim-lspconfig.lua
@@ -1,25 +1,61 @@
----@type MeoSpec
-return {
-    "neovim/nvim-lspconfig",
-    event = "LazyFile",
-    config = function()
-        ---@module "lspconfig"
-        ---@type lspconfig.Config
-        ---@diagnostic disable-next-line:missing-fields
-        local servers = {
-            lua_ls = {
-                settings = {
-                    Lua = {
-                        workspace = { checkThirdParty = false },
-                        completion = { callSnippet = "Replace" },
+local config = function()
+    ---@module "lspconfig"
+    ---@type lspconfig.Config
+    ---@diagnostic disable-next-line:missing-fields
+    local servers = {
+        basedpyright = {},
+        bashls = {},
+        clangd = { cmd = { "clangd", "--offset-encoding=utf-16" } },
+        cssls = {},
+        denols = {},
+        eslint = {},
+        gopls = {},
+        hls = {},
+        jsonls = {
+            settings = { json = { validate = { enable = true } } },
+            on_attach = function(client)
+                client.notify("workspace/didChangeConfiguration", {
+                    settings = {
+                        json = { schemas = require("schemastore").json.schemas() },
                     },
+                })
+            end,
+        },
+        lua_ls = {
+            settings = {
+                Lua = {
+                    workspace = { checkThirdParty = false },
+                    completion = { callSnippet = "Replace" },
                 },
             },
-        }
+        },
+        nil_ls = {},
+        ruff = {},
+        taplo = {},
+        tailwindcss = {},
+        texlab = {},
+        ts_ls = {
+            root_dir = function(...) require("lspconfig.util").root_pattern("package.json")(...) end,
+            single_file_support = false,
+        },
+        typos_lsp = {},
+        yamlls = {
+            settings = { yaml = { validate = true, keyOrdering = false } },
+            on_attach = function(client)
+                client.notify("workspace/didChangeConfiguration", {
+                    settings = {
+                        yaml = { schemas = require("schemastore").yaml.schemas() },
+                    },
+                })
+            end,
+        },
+    }
 
-        for name, config in pairs(servers) do
-            vim.lsp.config(name, config)
-            vim.lsp.enable(name)
-        end
-    end,
-}
+    for name, config in pairs(servers) do
+        vim.lsp.config(name, config)
+        vim.lsp.enable(name)
+    end
+end
+
+---@type MeoSpec
+return { "neovim/nvim-lspconfig", event = "LazyFile", config = config }
--- lua/meowim/plugins/rustaceanim.lua
+++ lua/meowim/plugins/rustaceanim.lua
@@ -2,5 +2,28 @@
 return {
     "mrcjkb/rustaceanvim",
     event = "LazyFile",
-    config = function() vim.g.rustaceanvim = { server = {} } end,
+    config = function()
+        local rust_analyzer = {
+            on_attach = function(_, bufnr)
+                Meow.keyset(bufnr, {
+                    { "<Leader>lm", "<Cmd>RustLsp expandMacro<CR>", desc = "Expand macro" },
+                    { "<Leader>lo", "<Cmd>RustLsp openCargo<CR>", desc = "Open Cargo.toml" },
+                })
+            end,
+            settings = {
+                ["rust-analyzer"] = {
+                    check = { command = "clippy" },
+                    rustfmt = { overrideCommand = { "rustfmt-nightly" } },
+                    procMacro = { enable = true, attributes = { enable = true } },
+                    typing = { autoClosingAngleBrackets = { enable = true } },
+                    imports = {
+                        granularity = { enforce = true, group = "module" },
+                        prefix = "self",
+                    },
+                    buildScripts = { rebuildOnSave = true },
+                },
+            },
+        }
+        vim.g.rustaceanvim = { server = rust_analyzer }
+    end,
 }
