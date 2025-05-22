3fd7f97ad5c255fe46f460c68a3d8d0a2953dfb4
Loi Chyan <loichyan@foxmail.com>
2025-05-22 14:51:06 +0800

    fix: stop binding `gX` to open URIs
    mini.operators has supported this functionality.

--- lua/meowim/plugins/mini/operators.lua
+++ lua/meowim/plugins/mini/operators.lua
@@ -11,27 +11,5 @@ return {
             replace  = { prefix = "gr" },
             sort     = { prefix = "gs" },
         })
-
-        -- Use 'gX' instead of Neovim's builtin 'gx' to open URIs.
-        Meow.keyset({
-            {
-                "gX",
-                function() vim.ui.open(vim.fn.expand("<cfile>")) end,
-                desc = "Open URI under cursor",
-            },
-            {
-                "gX",
-                function()
-                    local lines = vim.fn.getregion(
-                        vim.fn.getpos("."),
-                        vim.fn.getpos("v"),
-                        { type = vim.fn.mode() }
-                    )
-                    vim.ui.open(table.concat(vim.tbl_map(vim.trim, lines)))
-                end,
-                mode = "x",
-                desc = "Open selected URI",
-            },
-        })
     end,
 }
