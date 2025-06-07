eb501c67e773b46d3c986a6102d09f1d61b8e9ba
Loi Chyan <loichyan@foxmail.com>
2025-06-07 17:51:48 +0800

    refactor(mini.pick): move extra pickers to a standalone module

--- lua/meowim/plugins/mini/pick.lua
+++ lua/meowim/plugins/mini/pick.lua
@@ -1,7 +1,3 @@
-local get_config = function()
-    return vim.tbl_deep_extend("force", MiniPick.config, vim.b.minipick_config or {})
-end
-
 ---Shows preview at center.
 local center_preview = function(bufnr, item, opts)
     return MiniPick.default_preview(
@@ -9,143 +5,6 @@ local center_preview = function(bufnr, item, opts)
         item,
         vim.tbl_extend("force", { line_position = "center" }, opts or {})
     )
-end
-
----Shows items with icons.
-local show_with_icons = function(bufnr, items, query, opts)
-    return MiniPick.default_show(
-        bufnr,
-        items,
-        query,
-        vim.tbl_extend("force", { show_icons = true }, opts or {})
-    )
-end
-
----Lists files with a sensible picker.
----@param local_opts? {hidden:boolean}
-local pick_smart_files = function(local_opts, opts)
-    local_opts = vim.tbl_extend("force", { hidden = false }, local_opts or {})
-    local cwd = vim.fn.getcwd()
-    local command
-    if local_opts.hidden then
-        command = { "rg", "--files", "--no-follow", "--color=never", "--no-ignore" }
-        -- MiniExtra.pickers.git_files(local_opts, opts)
-    elseif require("meowim.utils").get_git_repo(cwd) == cwd then
-        -- stylua: ignore
-        command = { "git", "ls-files", "--exclude-standard", "--cached", "--modified", "--others", "--deduplicate" }
-    else
-        command = { "rg", "--files", "--no-follow", "--color=never" }
-    end
-
-    opts = vim.tbl_deep_extend("force", {
-        source = {
-            name = "Files",
-            cwd = cwd,
-            show = (get_config().source or {}).show or show_with_icons,
-        },
-    }, opts or {})
-    MiniPick.builtin.cli({ command = command }, opts)
-end
-
----Lists all todo comments of the specified keywords.
----@param opts? {scope:"current"|"all",keywords:string[]}
-local pick_todo = function(opts)
-    opts = vim.tbl_extend("force", { keywords = { "TODO", "FIXME" } }, opts or {})
-    local keywords = table.concat(opts.keywords, "|")
-    require("mini.pick").builtin.grep({
-        pattern = "\\b(" .. keywords .. ")(\\(.*\\))?:\\s+.+",
-        globs = opts.scope == "current" and { vim.fn.expand("%") } or nil,
-    }, {
-        source = { name = keywords },
-    })
-end
-
----Lists notifications from mini.notify.
--- TODO: contribute to mini.extra
----@param local_opts {source:string[]}
-local pick_notify = function(local_opts, opts)
-    local_opts = vim.tbl_extend("force", { source = { "vim.notify" } }, local_opts or {})
-
-    local notify = require("mini.notify")
-    local notify_config = vim.tbl_deep_extend("force", notify.config, vim.b.mininotify_config or {})
-    local format = notify_config.format or notify.default_format
-
-    local items = {}
-    for _, notif in pairs(notify.get_all()) do
-        if vim.tbl_contains(local_opts.source, notif.data.source) then
-            table.insert(items, { _orig = notif, text = vim.split(format(notif), "\n")[1] })
-        end
-    end
-    -- Sort from oldest to newest.
-    table.sort(items, function(a, b) return a._orig.ts_update < b._orig.ts_update end)
-
-    opts = vim.tbl_deep_extend("force", {
-        source = {
-            name = "Notifications",
-            items = items,
-            preview = function(buf_id, item)
-                vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, vim.split(item._orig.msg, "\n"))
-            end,
-            choose = function(item)
-                vim.schedule(function() vim.print(item._orig) end)
-            end,
-        },
-    }, opts or {})
-    MiniPick.start(opts)
-end
-
----Lists registered autocmds.
--- TODO: contribute to mini.extra
-local pick_autocmds = function(local_opts, opts)
-    -- stylua: ignore
-    local fields = {
-        { "group_name", "%-30s" },
-        { "event",      "%-20s" },
-        { "pattern",    "%-25s" },
-        { "desc",       "%s"    },
-    }
-    local format = function(autocmd)
-        return table.concat(
-            vim.tbl_map(function(f) return string.format(f[2], autocmd[f[1]] or "") end, fields)
-        )
-    end
-
-    local items = vim.tbl_map(
-        function(autocmd) return { _orig = autocmd, text = format(autocmd) } end,
-        vim.api.nvim_get_autocmds(local_opts or {})
-    )
-    -- Sort by group, then event
-    table.sort(items, function(a, b)
-        if not a._orig.group then
-            return false
-        elseif not b._orig.group then
-            return true
-        elseif a._orig.group == b._orig.group then
-            return a._orig.event < b._orig.event
-        else
-            return a._orig.group < b._orig.group
-        end
-    end)
-
-    opts = vim.tbl_deep_extend("force", {
-        source = {
-            name = "Autocmds",
-            items = items,
-            preview = function(buf_id, item)
-                vim.api.nvim_buf_set_lines(
-                    buf_id,
-                    0,
-                    -1,
-                    false,
-                    vim.split(vim.inspect(item._orig), "\n")
-                )
-            end,
-            choose = function(item)
-                vim.schedule(function() vim.print(item._orig) end)
-            end,
-        },
-    }, opts or {})
-    MiniPick.start(opts)
 end
 
 ---Shows all selected items in the quickfix list.
@@ -180,10 +39,9 @@ local config = function()
 
     -- Register extra pickers.
     Meow.load("mini.extra")
-    pick.registry.autocmds = pick_autocmds
-    pick.registry.notify = pick_notify
-    pick.registry.smart_files = pick_smart_files
-    pick.registry.todo = pick_todo
+    for name, picker in pairs(require("meowim.plugins.mini.extra.pickers")) do
+        pick.registry[name] = picker
+    end
 end
 
 ---@type MeoSpec
