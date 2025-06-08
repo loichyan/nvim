3b0be8f7705db5a1c64f48bb684cd5a3fd9c893b
Loi Chyan <loichyan@foxmail.com>
2025-06-08 21:46:43 +0800

    feat(polish): add a command to show command output

--- lua/meowim/config/polish.lua
+++ lua/meowim/config/polish.lua
@@ -11,3 +11,12 @@ vim.diagnostic.config({
     virtual_text = { current_line = false },
     virtual_lines = { current_line = true },
 })
+
+vim.api.nvim_create_user_command("Cat", function(ctx)
+    local output = vim.api.nvim_exec2(ctx.args, { output = true }).output
+    local lines = vim.split(output, "\n", { plain = true })
+    vim.cmd("tab new")
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+    vim.opt_local.filetype = "nofile"
+    vim.opt_local.modified = false
+end, { nargs = "+", complete = "command" })
