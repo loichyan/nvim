2708a84c3bbd3b9526023318a32379676b0b8a15
Loi Chyan <loichyan@foxmail.com>
2025-06-17 15:24:32 +0800

    fix(mini.hipatterns): remove unexpected spaces in keywords

--- lua/meowim/plugins/mini/hipatterns.lua
+++ lua/meowim/plugins/mini/hipatterns.lua
@@ -8,7 +8,7 @@ return {
             -- stylua: ignore
             highlighters = {
                 hex_color = hipatterns.gen_highlighter.hex_color(),
-                fixme = { pattern = Utils.hipattern_todo({ "FIXME "}), group = "MiniHipatternsFixme" },
+                fixme = { pattern = Utils.hipattern_todo({ "FIXME"}), group = "MiniHipatternsFixme" },
                 hack  = { pattern = Utils.hipattern_todo({ "HACK" }),  group = "MiniHipatternsHack"  },
                 todo  = { pattern = Utils.hipattern_todo({ "TODO" }),  group = "MiniHipatternsTodo"  },
                 note  = { pattern = Utils.hipattern_todo({ "NOTE" }),  group = "MiniHipatternsNote"  },
