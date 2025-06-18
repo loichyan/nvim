94003ed05ccf7fc673c052588b2819a731c8b5c0
Loi Chyan <loichyan@foxmail.com>
2025-06-18 00:48:41 +0000

    feat(snacks.quickfile): enable snacks.quickfile

--- lua/meowim/plugins/snacks.lua
+++ lua/meowim/plugins/snacks.lua
@@ -2,8 +2,13 @@
 return {
     "folke/snacks.nvim",
     event = "VeryLazy",
+    lazy = false,
+    priority = 90,
     config = function()
-        require("snacks").setup({ input = { enabled = true } })
+        require("snacks").setup({
+            quickfile = { enabled = true },
+            input = { enabled = true },
+        })
         vim.api.nvim_create_autocmd("User", {
             pattern = "MiniFilesActionRename",
             callback = function(ev) Snacks.rename.on_rename_file(ev.data.from, ev.data.to) end,
