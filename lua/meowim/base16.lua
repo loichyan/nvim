b3556baf029dbb173b724d19b04f64dddf934e22
Loi Chyan <loichyan@foxmail.com>
2025-06-28 21:45:55 +0800

    feat(base16): make hunk previews easier to recognize

--- lua/meowim/base16.lua
+++ lua/meowim/base16.lua
@@ -105,12 +105,19 @@ function Base16.colors_customizations(opts, colors)
         ["MiniIndentscopeSymbol"]    = { fg = palette.base04 },
         ["MiniIndentscopeSymbolOff"] = { fg = palette.base04 },
 
-        ["GitConflictCurrent"]       = { bg = lighten(palette.base0D, -37) },
-        ["GitConflictCurrentLabel"]  = { bg = lighten(palette.base0D, -17) },
-        ["GitConflictAncestor"]      = { bg = lighten(palette.base0E, -37) },
-        ["GitConflictAncestorLabel"] = { bg = lighten(palette.base0E, -17) },
-        ["GitConflictIncoming"]      = { bg = lighten(palette.base0B, -37) },
-        ["GitConflictIncomingLabel"] = { bg = lighten(palette.base0B, -17) },
+        ["GitConflictCurrent"]       = { fg = palette.base05, bg = lighten(palette.base0D, -37) },
+        ["GitConflictCurrentLabel"]  = { fg = palette.base05, bg = lighten(palette.base0D, -23) },
+        ["GitConflictAncestor"]      = { fg = palette.base05, bg = lighten(palette.base0E, -37) },
+        ["GitConflictAncestorLabel"] = { fg = palette.base05, bg = lighten(palette.base0E, -23) },
+        ["GitConflictIncoming"]      = { fg = palette.base05, bg = lighten(palette.base0B, -37) },
+        ["GitConflictIncomingLabel"] = { fg = palette.base05, bg = lighten(palette.base0B, -23) },
+
+        ["MiniDiffOverAdd"]        = { fg = palette.base05, bg = lighten(palette.base0B, -37) },
+        ["MiniDiffOverDelete"]     = { fg = palette.base05, bg = lighten(palette.base08, -37) },
+        ["MiniDiffOverChange"]     = { fg = palette.base05, bg = lighten(palette.base08, -37) },
+        ["MiniDiffOverChangeBuf"]  = { fg = palette.base05, bg = lighten(palette.base0B, -37) },
+        ["MiniDiffOverContext"]    = { bg = palette.base01, fg = lighten(palette.base08, -23) },
+        ["MiniDiffOverContextBuf"] = {},
     }
     for name, hl in pairs(overrides) do
         colors.groups[name] = hl
