89a85f6583057faa5da25b36aaa97b84f1f16b1d
Loi Chyan <loichyan@foxmail.com>
2025-07-09 21:58:21 +0800

    fix(mini.completion): lower the priority of 'text' completion items

--- lua/meowim/plugins/mini/completion.lua
+++ lua/meowim/plugins/mini/completion.lua
@@ -3,11 +3,21 @@ return {
     "mini.completion",
     event = "LazyFile",
     config = function()
-        vim.o.completeopt = "menuone,noinsert,fuzzy"
         local completion = require("mini.completion")
+
+        local kind_priority = { Text = 1, Snippet = 99 }
+        local opts = { kind_priority = kind_priority }
+        local process_items = function(items, base)
+            return MiniCompletion.default_process_items(items, base, opts)
+        end
+
+        vim.o.completeopt = "menuone,noinsert,fuzzy"
         completion.setup({
-            -- 'completefunc' conflicts with `Ctrl+U` in insert mode
-            lsp_completion = { source_func = "omnifunc" },
+            lsp_completion = {
+                -- 'completefunc' conflicts with `Ctrl+U` in insert mode
+                source_func = "omnifunc",
+                process_items = process_items,
+            },
             window = {
                 info = { border = "solid" },
                 signature = { border = "solid" },
