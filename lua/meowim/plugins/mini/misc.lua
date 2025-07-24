e210a7152e965675a508e8debc8d0263bd1a9712
Loi Chyan <loichyan@foxmail.com>
2025-07-24 09:24:28 +0000

    fix(mini.misc): change root directoy only when opening a file

--- lua/meowim/plugins/mini/misc.lua
+++ lua/meowim/plugins/mini/misc.lua
@@ -5,8 +5,29 @@ return {
     config = function()
         local misc = require("mini.misc")
         misc.setup({ make_global = { "put", "put_text", "bench_time", "stat_summary" } })
-        misc.setup_auto_root({ ".git" })
         misc.setup_restore_cursor()
+
+        -- Setup auto root directory discovery
+        local root_patterns = { ".git" }
+        vim.o.autochdir = false
+        vim.api.nvim_create_autocmd("BufEnter", {
+            desc = "Find root and change current directory",
+            nested = true,
+            callback = vim.schedule_wrap(function(data)
+                if data.buf ~= vim.api.nvim_get_current_buf() then
+                    return
+                end
+                -- Ignore non-file buffers
+                if vim.bo.buftype ~= "" then
+                    return
+                end
+                local root = MiniMisc.find_root(data.buf, root_patterns)
+                if root ~= nil then
+                    vim.fn.chdir(root)
+                end
+            end),
+        })
+
         Meow.keyset({
             {
                 "<Leader>m",
