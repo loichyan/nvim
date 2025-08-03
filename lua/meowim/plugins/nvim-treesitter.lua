f15c6392eeb090111cf8d081b73e0ba3aa84e9ff
Loi Chyan <loichyan@foxmail.com>
2025-08-03 08:37:16 +0800

    fix(nvim-treesitter): re-enable the 'indent' module
    It has been verified that this was just a coincidence: the indent module
    works poorly only for plain text blocks, which can be addressed by
    disabling it temporarily and then turning on 'cindent'.
    
    This reverts commit fc7ec4e98cebbfa96f520ee560f1d5a78430f80a.

--- lua/meowim/plugins/nvim-treesitter.lua
+++ lua/meowim/plugins/nvim-treesitter.lua
@@ -21,6 +21,7 @@ local config = function()
     auto_install = true,
     highlight = { enable = true, additional_vim_regex_highlighting = false },
     incremental_selection = { enable = false },
+    indent = { enable = true },
     textobjects = {
       -- stylua: ignore
       move = {
