97a00193121629dfb9c773e885b0b8ebbfc31ae6
Loi Chyan <loichyan@foxmail.com>
2025-08-07 14:36:13 +0800

    fix: load autocommands early when opening files

--- lua/meowim/init.lua
+++ lua/meowim/init.lua
@@ -6,14 +6,24 @@ function Meowim.setup()
   did_setup = true
 
   require("meowim.config.options")
+
+  -- Load autocommands and polishment early if Vim is about to open files.
+  local has_file = vim.fn.argc(-1) > 0
+  if has_file then
+    require("meowim.config.polish")
+    require("meowim.config.autocmds")
+  end
+
   -- Setup keymaps and autocommands once we enter the UI.
-  vim.api.nvim_create_autocmd("UIEnter", {
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
     once = true,
     desc = "meowim.config",
     callback = function()
       require("meowim.config.keymaps")
-      require("meowim.config.autocmds")
+      if has_file then return end
       require("meowim.config.polish")
+      require("meowim.config.autocmds")
     end,
   })
   -- See <https://github.com/LazyVim/LazyVim/blob/ec5981dfb1222c3bf246d9bcaa713d5cfa486fbd/lua/lazyvim/util/plugin.lua#L10>
