32375d6e6978b5ea8d31d9ef952b1ee29420661d
Loi Chyan <loichyan@foxmail.com>
2025-08-13 06:50:05 +0800

    refactor(mini.diff): use `]gh` and `[gh` to jump across hunks
    `gh` matches the textobject `gh` better.
    
    BREAKING CHANGE: `]g` and `[g` have been remapped.

--- lua/meowim/plugins/mini/diff.lua
+++ lua/meowim/plugins/mini/diff.lua
@@ -8,10 +8,13 @@ Spec.config = function()
       apply      = "gh",
       reset      = "gH",
       textobject = "gh",
-      goto_first = "[G",
-      goto_prev  = "[g",
-      goto_next  = "]g",
-      goto_last  = "]G",
+      goto_prev  = "",
+      goto_next  = "",
+      goto_first = "",
+      goto_last  = "",
+    },
+    options = {
+      wrap_goto = true,
     },
   })
 
@@ -20,12 +23,18 @@ Spec.config = function()
   end
   local do_buffer = function(mode) require("mini.diff").do_hunks(0, mode) end
 
+  local nxo = { "n", "x", "o" }
   -- stylua: ignore
   Meow.keymap({
     { "ghh", function() return do_cursor("apply") end, expr = true, desc = "Stage cursor hunks" },
     { "gHh", function() return do_cursor("reset") end, expr = true, desc = "Reset cursor hunks" },
     { "ghH", function() return do_buffer("apply") end,              desc = "Stage buffer hunks" },
     { "gHH", function() return do_buffer("reset") end,              desc = "Reset buffer hunks" },
+
+    { "[gh", function() require("mini.diff").goto_hunk("prev") end,  mode = nxo, desc = "Hunk backward" },
+    { "[gH", function() require("mini.diff").goto_hunk("first") end, mode = nxo, desc = "Hunk first"    },
+    { "]gh", function() require("mini.diff").goto_hunk("next") end,  mode = nxo, desc = "Hunk forward"  },
+    { "]gH", function() require("mini.diff").goto_hunk("last") end,  mode = nxo, desc = "Hunk last"     },
   })
 end
 
