109c38ec7a9504970eca3c3fc98ca05d63fd9102
Loi Chyan <loichyan@foxmail.com>
2025-08-16 09:02:49 +0800

    feat(mini.completion)!: refine completion experience
    The new completion features the following design:
    
    1. Only the first keyword part of an item is used as the completion
       word, which means for a function item, only its name will be inserted
       when you select it through `<C-n>/<C-p>`.
    2. Only a few keys are intended to accept a completion. This is
       different from mini.completion, which accepts all non-keyword chars.

--- lua/meowim/plugins/mini/completion.lua
+++ lua/meowim/plugins/mini/completion.lua
@@ -4,19 +4,38 @@ local Spec = {
   event = "LazyFile",
   dependencies = { "mini.snippets" },
 }
+local H = {}
 
 Spec.config = function()
-  local completion = require("mini.completion")
+  local minicomp = require("mini.completion")
+
+  Meow.autocmd("meowim.plugins.mini.completion", {
+    {
+      -- Must be executed before mini.completion's autocommmands
+      event = "CompleteDonePre",
+      desc = "Filter out unintended confirms",
+      callback = function()
+        -- Only use certain keys to confirm a completion.
+        -- This resolves <https://github.com/echasnovski/mini.nvim/issues/1938>.
+        if not vim.g.meowim_complete_confirm then
+          vim.v.completed_item = vim.empty_dict()
+        else
+          vim.g.meowim_complete_confirm = nil
+        end
+      end,
+    },
+  })
 
   local kind_priority = { Text = 1, Snippet = 99 }
-  local opts = { kind_priority = kind_priority }
+  local process_opts = { kind_priority = kind_priority }
   local process_items = function(items, base)
-    return MiniCompletion.default_process_items(items, base, opts)
+    items = H.process_lsp_items(items)
+    return MiniCompletion.default_process_items(items, base, process_opts)
   end
 
-  vim.o.completeopt = "menuone,noinsert,fuzzy,nosort"
+  vim.o.completeopt = "menuone,noselect,fuzzy"
   vim.o.infercase = false
-  completion.setup({
+  minicomp.setup({
     lsp_completion = {
       -- 'completefunc' conflicts with `Ctrl+U` in insert mode
       source_func = "omnifunc",
@@ -34,7 +53,50 @@ Spec.config = function()
       scroll_up      = "<C-b>",
     },
   })
-  vim.lsp.config("*", { capabilities = completion.get_lsp_capabilities() })
+  vim.lsp.config("*", { capabilities = minicomp.get_lsp_capabilities() })
+end
+
+---@param items lsp.CompletionItem[]
+function H.process_lsp_items(items)
+  local item_kind = vim.lsp.protocol.CompletionItemKind
+  local textformat = vim.lsp.protocol.InsertTextFormat
+
+  -- For each item, use only its first keyword part as the completion word if
+  -- possible, which can increase the fuzzy-filtering accuracy and stop Nvim
+  -- from inserting too many useless characters when it gets selected.
+  for _, item in ipairs(items) do
+    -- For snippet items, respect the abbr/word/filterText anyway, since they
+    -- are configured intentionally.
+    if item.kind ~= item_kind.Snippet then
+      local abbr = item.label -- word shown in popupmenu
+      local word = abbr:match("^[-_.%w]+") or abbr -- word used to match
+      local textedit = (item.textEdit or {}).newText
+      local inserttext = textedit or item.insertText or abbr -- word to be inserted
+
+      item.filterText = word
+      item.sortText = word
+
+      if word == inserttext then
+        -- If the completion word matches the text to be inserted, do not make it
+        -- a potential snippet, since some LSPs report all items as snippets.
+        item.insertTextFormat = textformat.PlainText
+      else
+        -- Otherwise, ensure the new item can be recognized as a snippet by
+        -- mini.completion. The presence of at least one tabstop is important,
+        -- which resolves <https://github.com/echasnovski/mini.nvim/issues/1944>.
+        item.insertTextFormat = textformat.Snippet
+        local has_tabstop = inserttext:find("[^\\]%${?%w") or inserttext:find("^%${?%w")
+        if has_tabstop then
+        elseif textedit then
+          item.textEdit.newText = textedit .. "$0"
+        else
+          item.insertText = inserttext .. "$0"
+        end
+      end
+    end
+  end
+
+  return items
 end
 
 return Spec
