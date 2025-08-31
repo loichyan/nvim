51f54c7b714ef5d7f4728c97dde738ea2ad9ba4a
Loi Chyan <loichyan@foxmail.com>
2025-08-31 11:18:00 +0800

    refactor: move `meowim.lspconfig` into `meowim.config`

--- lua/meowim/plugins/nvim-lspconfig.lua
+++ lua/meowim/plugins/nvim-lspconfig.lua
@@ -2,7 +2,7 @@
 local Spec = { "neovim/nvim-lspconfig", event = "VeryLazy" }
 
 Spec.config = function()
-  for name, config in pairs(require("meowim.lspconfig")) do
+  for name, config in pairs(require("meowim.config.lspconfig")) do
     vim.lsp.config(name, config)
     if name ~= "rust_analyzer" and config.enable ~= false then vim.lsp.enable(name) end
   end
--- lua/meowim/plugins/rustaceanim.lua
+++ lua/meowim/plugins/rustaceanim.lua
@@ -2,7 +2,7 @@
 local Spec = { "mrcjkb/rustaceanvim", event = "LazyFile" }
 
 Spec.config = function()
-  local rust_analyzer = require("meowim.lspconfig").rust_analyzer
+  local rust_analyzer = require("meowim.config.lspconfig").rust_analyzer
   vim.g.rustaceanvim = {
     server = vim.tbl_extend("force", rust_analyzer, { auto_attach = rust_analyzer.enable }),
   }
