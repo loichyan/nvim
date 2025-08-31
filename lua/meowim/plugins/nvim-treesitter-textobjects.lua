7fcd8c60550a4df477d2cac6dcc92d327b46e182
Loi Chyan <loichyan@foxmail.com>
2025-08-31 10:41:25 +0800

    feat(treesitter): migrate to main branch of nvim-treesitter
    References:
    - https://github.com/cameronr/dotfiles/blob/c83f1dd58484814c18f38634840b5a6b61c6a7d3/nvim/lua/plugins/treesitter.lua
    - https://github.com/luisdavim/dotfiles/blob/2b16601ac18749de9a8d4eeca9391d8d37be3b7a/files/config/nvim/init.lua
    
    BREAKING CHANGE: nvim-treesitter of the main branch should be considered
    as a different plugin, which implies breakages in the current codebase.

--- lua/meowim/plugins/nvim-treesitter-textobjects.lua
+++ lua/meowim/plugins/nvim-treesitter-textobjects.lua
@@ -1,3 +1,31 @@
 ---@type MeoSpec
--- TODO: migrate to main branch when nvim-treesitter-textobjects is ready to do so
-return { "nvim-treesitter/nvim-treesitter-textobjects", checkout = "master", lazy = true }
+local Spec = {
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  checkout = "main",
+  event = "LazyFile",
+}
+
+Spec.config = function()
+  require("nvim-treesitter-textobjects").setup()
+
+  local move = function(action, target) require("nvim-treesitter-textobjects.move")[action](target) end
+  -- stylua: ignore
+  Meow.keymap({
+    { "]a", function() move("goto_next_start",     "@parameter.inner") end, desc = "Parameter start forward"  },
+    { "]A", function() move("goto_next_end",       "@parameter.inner") end, desc = "Parameter end forward"    },
+    { "[a", function() move("goto_previous_start", "@parameter.inner") end, desc = "Parameter start backward" },
+    { "[A", function() move("goto_previous_end",   "@parameter.inner") end, desc = "Parameter end backward"   },
+
+    { "]c", function() move("goto_next_start",     "@class.outer")     end, desc = "Class start forward"      },
+    { "]C", function() move("goto_next_end",       "@class.outer")     end, desc = "Class end forward"        },
+    { "[c", function() move("goto_previous_start", "@class.outer")     end, desc = "Class start backward"     },
+    { "[C", function() move("goto_previous_end",   "@class.outer")     end, desc = "Class end backward"       },
+
+    { "]f", function() move("goto_next_start",     "@function.outer")  end, desc = "Function start forward"   },
+    { "]F", function() move("goto_next_end",       "@function.outer")  end, desc = "Function end forward"     },
+    { "[f", function() move("goto_previous_start", "@function.outer")  end, desc = "Function start backward"  },
+    { "[F", function() move("goto_previous_end",   "@function.outer")  end, desc = "Function end backward"    },
+  })
+end
+
+return Spec
--- lua/meowim/plugins/nvim-treesitter.lua
+++ lua/meowim/plugins/nvim-treesitter.lua
@@ -1,46 +1,83 @@
 ---@type MeoSpec
 local Spec = {
   "nvim-treesitter/nvim-treesitter",
-  checkout = "master",
+  checkout = "main",
   build = function() vim.cmd("TSUpdate") end,
-  event = "VeryLazy",
+  lazy = false,
 }
+local H = {}
 
 Spec.config = function()
   -- See <https://github.com/neovim/neovim/issues/32660>
   vim.g._ts_force_sync_parsing = true
+  require("nvim-treesitter").setup()
 
-  ---@diagnostic disable-next-line:missing-fields
-  require("nvim-treesitter.configs").setup({
-    sync_install = false,
-    ensure_installed = {
-      "bash",
-      "c",
-      "diff",
-      "html",
-      "lua",
-      "luadoc",
-      "markdown",
-      "markdown_inline",
-      "query",
-      "vim",
-      "vimdoc",
+  Meow.autocmd("meowim.plugins.nvim-treesitter", {
+    {
+      event = "User",
+      pattern = "VeryLazy",
+      desc = "Ensure some parsers are installed",
+      once = true,
+      callback = vim.schedule_wrap(H.ensure_installed),
     },
-    auto_install = true,
-    highlight = { enable = true, additional_vim_regex_highlighting = false },
-    incremental_selection = { enable = false },
-    indent = { enable = true },
-    textobjects = {
-      -- stylua: ignore
-      move = {
-        enable = true,
-        goto_next_start     = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-        goto_next_end       = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-        goto_previous_end   = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-      },
+    {
+      event = "FileType",
+      desc = "Auto install parsers",
+      callback = H.setup_parser,
     },
   })
+end
+
+function H.ensure_installed()
+  local ensure_installed = {
+    "bash",
+    "c",
+    "css",
+    "diff",
+    "gitcommit",
+    "html",
+    "lua",
+    "luadoc",
+    "markdown",
+    "markdown_inline",
+    "query",
+    "vim",
+    "vimdoc",
+  }
+  require("nvim-treesitter").install(ensure_installed)
+end
+
+function H.setup_parser(ev)
+  local parser = vim.treesitter.language.get_lang(ev.match)
+  if not H.is_available(parser) then return end
+
+  local bufnr, winnr = vim.api.nvim_get_current_buf(), vim.api.nvim_get_current_win()
+  require("nvim-treesitter").install({ parser }):await(function(err)
+    if err then
+      Meow.notifyf("ERROR", "failed to install parser '%s': %s", parser, err)
+      return
+    end
+
+    pcall(function() -- ignore errors when window/buffer gets invalid
+      vim.treesitter.start(bufnr)
+      vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+      -- TODO: enable LSP foldexpr if supported
+      vim.wo[winnr].foldlevel = 99
+      vim.wo[winnr].foldmethod = "expr"
+      vim.wo[winnr].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    end)
+  end)
+end
+
+function H.is_available(parser)
+  if not H.available then
+    H.available = {}
+    for _, p in ipairs(require("nvim-treesitter.config").get_available()) do
+      H.available[p] = true
+    end
+  end
+  return H.available[parser]
 end
 
 return Spec
