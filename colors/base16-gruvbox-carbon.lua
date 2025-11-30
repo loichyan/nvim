d81e7cefecaa3c023915332b7a2b948c82c2c4dc
Loi Chyan <loichyan@foxmail.com>
2025-11-30 14:22:02 +0800

    feat(colors): resolve badly contrasted light colors

--- colors/base16-gruvbox-carbon.lua
+++ colors/base16-gruvbox-carbon.lua
@@ -1,4 +1,5 @@
 local palette
+
 if vim.o.background ~= "light" then
   -- Scheme: Gruvbox Carbon Dark
   -- Author: Sainnhe Park (https://github.com/sainnhe), Loi Chyan (https://github.com/loichyan)
@@ -38,7 +39,7 @@ else
 
     base08 = "#9d0006",
     base09 = "#af3a03",
-    base0A = "#b57614",
+    base0A = "#a26700",
     base0B = "#79740e",
     base0C = "#427b58",
     base0D = "#076678",
--- colors/base16-gruvbox-material.lua
+++ colors/base16-gruvbox-material.lua
@@ -38,7 +38,7 @@ else
 
     base08 = "#9d0006",
     base09 = "#af3a03",
-    base0A = "#b57614",
+    base0A = "#a26700",
     base0B = "#79740e",
     base0C = "#427b58",
     base0D = "#076678",
