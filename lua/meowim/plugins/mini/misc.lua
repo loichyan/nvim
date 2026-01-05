c625f1b80642c580682964660a7dc853f083c1ef
Loi Chyan <loichyan@outlook.com>
2026-01-05 10:35:44 +0800

    feat(mini.misc): disable 'auto_root'
    It can be annoying when editing a temporary file.

--- lua/meowim/plugins/mini/misc.lua
+++ lua/meowim/plugins/mini/misc.lua
@@ -6,16 +6,6 @@ Spec.config = function()
   local minimisc = require("mini.misc")
   minimisc.setup({ make_global = { "put", "put_text", "bench_time", "stat_summary" } })
   minimisc.setup_restore_cursor()
-  minimisc.setup_auto_root({ ".git" })
-
-  -- TODO: report to mini.misc?
-  -- Ensure target for `find_root` is a valid buffer
-  ---@diagnostic disable-next-line: duplicate-set-field
-  minimisc.find_root = Meowim.utils.wrap_fn(minimisc.find_root, function(find_root, bufnr, ...)
-    bufnr = bufnr or 0
-    if vim.bo[bufnr].buftype ~= "" then return end
-    return find_root(bufnr, ...)
-  end)
 end
 
 ---Zooms in/out current buffer.
