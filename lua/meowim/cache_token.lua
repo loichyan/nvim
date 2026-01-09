0975dd2f797b5892092c470542deb324729118b1
Loi Chyan <loichyan@outlook.com>
2026-01-09 11:44:34 +0800

    feat(meowim): notify when cache_token expires

--- lua/meowim/cache_token.lua
+++ lua/meowim/cache_token.lua
@@ -2,14 +2,19 @@ if vim.env["MEO_DISABLE_CACHE"] then return false end
 
 local config_dir = vim.fn.stdpath("config")
 local token_path = config_dir .. "/cache_token"
+local token_file = io.open(token_path, "r")
+local token = token_file and token_file:read("*a") or nil
 
 -- Refresh token in the background
 vim.schedule(function()
   vim.system({ "git", "-C", config_dir, "rev-parse", "HEAD" }, nil, function(out)
-    local sha = vim.trim(out.stdout or "")
-    if sha ~= "" then io.open(token_path, "w"):write(out.stdout) end
+    local new_token = vim.trim(out.stdout or "")
+    if new_token == token then return end
+    io.open(token_path, "w"):write(new_token)
+    if token ~= nil then
+      Meow.notify("WARN", "Meowim has been updated. Please restart to apply the changes.")
+    end
   end)
 end)
 
-local f = io.open(token_path, "r")
-return f and f:read("*a") or tostring(math.random(0, 0xffffffff))
+return token or tostring(math.random(0, 0xffffffff))
