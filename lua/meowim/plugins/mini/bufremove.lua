a26720e983e32162cb7c09b31e7b2d86fa23fd88
Loi Chyan <loichyan@outlook.com>
2026-02-09 15:32:42 +0800

    feat(keymaps): use apis provided by meoline.nvim to manage buffers

--- lua/meowim/plugins/mini/bufremove.lua
+++ lua/meowim/plugins/mini/bufremove.lua
@@ -1,22 +1,6 @@
 ---@type MeoSpec
-local Spec = { 'mini.bufremove', lazy = true }
-local M = { Spec }
-
-Spec.config = function() require('mini.bufremove').setup() end
-
----Close other buffers.
----@param dir 'left'|'right'|'all'
-M.close_others = function(dir)
-  local idir = dir == 'left' and -1 or dir == 'right' and 1 or 0
-  local curr = vim.api.nvim_get_current_buf()
-  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-    if curr == bufnr then
-      if idir < 0 then break end
-      idir = 0
-    elseif vim.bo[bufnr].buflisted and idir <= 0 then
-      require('mini.bufremove').delete(bufnr)
-    end
-  end
-end
-
-return M
+return {
+  'mini.bufremove',
+  lazy = true,
+  config = function() require('mini.bufremove').setup() end,
+}
