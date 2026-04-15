2ced6837548efd080aadf2b83fad640ea5ba9230
Loi Chyan <loichyan@outlook.com>
2026-04-15 15:05:12 +0800

    fix(rustaceanvim): resolve wrong server configurations

--- lua/meowim/plugins/rustaceanvim.lua
+++ lua/meowim/plugins/rustaceanvim.lua
@@ -3,12 +3,12 @@ local Spec = { 'mrcjkb/rustaceanvim', event = 'LazyFile' }
 
 Spec.config = function()
   Meow.load('nvim-lspconfig')
-  local lspconfig = vim.lsp.config['rust_analyzer']
+  local lspconfig = vim.lsp.config['rust_analyzer'] --[[@cast lspconfig -nil]]
   vim.g.rustaceanvim = {
     server = {
       auto_attach = true,
       capabilities = lspconfig.capabilities,
-      on_attach = lspconfig.settings,
+      on_attach = lspconfig.on_attach,
       default_settings = lspconfig.settings,
     },
   }
