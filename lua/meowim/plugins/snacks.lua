5b75d4ed73817ba61d2f393d6e9491333cd70fb7
Loi Chyan <loichyan@outlook.com>
2026-06-22 20:09:24 +0800

    feat(mini.files): prefer mini.files's builtin LSP integration

--- lua/meowim/plugins/snacks.lua
+++ lua/meowim/plugins/snacks.lua
@@ -15,14 +15,6 @@ Spec.config = function()
       filekey = { branch = false },
     },
   })
-  Meow.autocmd('meowim.plugins.snacks', {
-    {
-      event = 'User',
-      pattern = 'MiniFilesActionRename',
-      desc = 'Track renamed files',
-      callback = function(ev) require('snacks.rename').on_rename_file(ev.data.from, ev.data.to) end,
-    },
-  })
 end
 
 return Spec
